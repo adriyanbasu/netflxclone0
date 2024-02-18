f003888ff05c80ae3f9d99803bbf8454a03c449a
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-18 17:33:15 +0000

    commiting to

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/320px-Netflix_2015_logo.svg.png",
+        port: "",
+        pathname: "/account123/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
